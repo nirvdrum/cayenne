2879499b70a9e2426be4d14e073b3fa9a39f1921
Andrus Adamchik <aadamchik@apache.org>
2002-07-16 16:41:09 +0000

    preparing web tutorial
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@400969 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/doc/tutorial/cayenne-web-app/WEB-INF/classes/test/Gallery.java
+++ cayenne/doc/tutorial/cayenne-web-app/WEB-INF/classes/test/Gallery.java
@@ -1,29 +1,9 @@
 package test;
 
-import java.util.List;
 import org.objectstyle.cayenne.*;
 
-public class Gallery extends CayenneDataObject {
+public class Gallery extends _Gallery {
 
-    public void setGalleryName(java.lang.String galleryName) {
-        writeProperty("galleryName", galleryName);
-    }
-    public java.lang.String getGalleryName() {
-        return (java.lang.String)readProperty("galleryName");
-    }
-    
-    
-    public void addToPaintingArray(test.Painting obj) {
-        addToManyTarget("paintingArray", obj, true);
-    }
-    public void removeFromPaintingArray(test.Painting obj) {
-        removeToManyTarget("paintingArray", obj, true);
-    }
-    public List getPaintingArray() {
-        return (List)readProperty("paintingArray");
-    }
-    
-    
 }
 
 
