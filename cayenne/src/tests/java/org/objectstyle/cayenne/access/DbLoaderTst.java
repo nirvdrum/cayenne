65df0e0e7e732188a0528e4797b8a6ad4f76654e
Andrus Adamchik <aadamchik@apache.org>
2002-10-30 05:57:21 +0000

    updated for the new constructor
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@401334 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/src/tests/java/org/objectstyle/cayenne/access/DbLoaderTst.java
+++ cayenne/src/tests/java/org/objectstyle/cayenne/access/DbLoaderTst.java
@@ -81,7 +81,7 @@ public class DbLoaderTst extends CayenneTestCase {
 		loader =
 			new DbLoader(
 				getSharedConnection(),
-				getSharedNode().getAdapter());
+				getSharedNode().getAdapter(), null);
 	}
 
 	/** 
