1cf3d5680361cf0207a7bcc3bbe1b34e0a98542f
Andrus Adamchik <aadamchik@apache.org>
2002-11-05 02:39:14 +0000

    formatted code. Made class public (it had default access for no reason)
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@401345 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/src/modeler/java/org/objectstyle/cayenne/modeler/EditorView.java
+++ cayenne/src/modeler/java/org/objectstyle/cayenne/modeler/EditorView.java
@@ -54,35 +54,49 @@ package org.objectstyle.cayenne.modeler;
  * information on the ObjectStyle Group, please see
  * <http://objectstyle.org/>.
  *
- */ 
+ */
 
-
-
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 
 import org.objectstyle.cayenne.modeler.datamap.DbDetailView;
 import org.objectstyle.cayenne.modeler.datamap.ObjDetailView;
-import org.objectstyle.cayenne.modeler.event.*;
+import org.objectstyle.cayenne.modeler.event.DataMapDisplayEvent;
+import org.objectstyle.cayenne.modeler.event.DataMapDisplayListener;
+import org.objectstyle.cayenne.modeler.event.DataNodeDisplayEvent;
+import org.objectstyle.cayenne.modeler.event.DataNodeDisplayListener;
+import org.objectstyle.cayenne.modeler.event.DbEntityDisplayListener;
+import org.objectstyle.cayenne.modeler.event.DomainDisplayEvent;
+import org.objectstyle.cayenne.modeler.event.DomainDisplayListener;
+import org.objectstyle.cayenne.modeler.event.EntityDisplayEvent;
+import org.objectstyle.cayenne.modeler.event.Mediator;
+import org.objectstyle.cayenne.modeler.event.ObjEntityDisplayListener;
 
 /** Panel for the Editor window. 
  *  @author Michael Misha Shengaout. */
-class EditorView extends JPanel 
-implements ObjEntityDisplayListener, DbEntityDisplayListener
-, DomainDisplayListener, DataMapDisplayListener, DataNodeDisplayListener {
+public class EditorView
+    extends JPanel
+    implements
+        ObjEntityDisplayListener,
+        DbEntityDisplayListener,
+        DomainDisplayListener,
+        DataMapDisplayListener,
+        DataNodeDisplayListener {
     Mediator mediator;
 
     private static final int INIT_DIVIDER_LOCATION = 170;
 
-    private static final String EMPTY_VIEW    = "Empty";
-    private static final String DOMAIN_VIEW   = "Domain";
-    private static final String NODE_VIEW   = "Node";
+    private static final String EMPTY_VIEW = "Empty";
+    private static final String DOMAIN_VIEW = "Domain";
+    private static final String NODE_VIEW = "Node";
     private static final String DATA_MAP_VIEW = "DataMap";
-    private static final String OBJ_VIEW    = "ObjView";
-    private static final String DB_VIEW    = "DbView";
-
+    private static final String OBJ_VIEW = "ObjView";
+    private static final String DB_VIEW = "DbView";
 
     JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
     BrowseView treePanel;
@@ -102,30 +116,30 @@ implements ObjEntityDisplayListener, DbEntityDisplayListener
         add(splitPane, BorderLayout.CENTER);
         treePanel = new BrowseView(temp_mediator);
         splitPane.setLeftComponent(treePanel);
-	    splitPane.setRightComponent(detailPanel);
+        splitPane.setRightComponent(detailPanel);
 
-		Dimension minimumSize = new Dimension(350, 200);
-	    detailPanel.setMinimumSize(minimumSize);
-		minimumSize = new Dimension(170, 200);
-		treePanel.setMinimumSize(minimumSize);
+        Dimension minimumSize = new Dimension(350, 200);
+        detailPanel.setMinimumSize(minimumSize);
+        minimumSize = new Dimension(170, 200);
+        treePanel.setMinimumSize(minimumSize);
 
         detailLayout = new CardLayout();
         detailPanel.setLayout(detailLayout);
 
-		JPanel temp = new JPanel(new FlowLayout());
-		detailPanel.add(temp, EMPTY_VIEW);
-		domainView = new DomainDetailView(temp_mediator);
-		detailPanel.add(domainView, DOMAIN_VIEW);
-		nodeView = new DataNodeDetailView(temp_mediator);
-		detailPanel.add(nodeView, NODE_VIEW);
-		dataMapView = new DataMapDetailView(temp_mediator);
-		detailPanel.add(dataMapView, DATA_MAP_VIEW);
-		
+        JPanel temp = new JPanel(new FlowLayout());
+        detailPanel.add(temp, EMPTY_VIEW);
+        domainView = new DomainDetailView(temp_mediator);
+        detailPanel.add(domainView, DOMAIN_VIEW);
+        nodeView = new DataNodeDetailView(temp_mediator);
+        detailPanel.add(nodeView, NODE_VIEW);
+        dataMapView = new DataMapDetailView(temp_mediator);
+        detailPanel.add(dataMapView, DATA_MAP_VIEW);
+
         objDetailView = new ObjDetailView(temp_mediator);
         detailPanel.add(objDetailView, OBJ_VIEW);
         dbDetailView = new DbDetailView(temp_mediator);
         detailPanel.add(dbDetailView, DB_VIEW);
-        
+
         mediator.addDomainDisplayListener(this);
         mediator.addDataNodeDisplayListener(this);
         mediator.addDataMapDisplayListener(this);
@@ -133,45 +147,38 @@ implements ObjEntityDisplayListener, DbEntityDisplayListener
         mediator.addDbEntityDisplayListener(this);
     }
 
-   	public void currentDomainChanged(DomainDisplayEvent e)
-   	{
-   		if (e.getDomain() == null)
-	   		detailLayout.show(detailPanel, EMPTY_VIEW);
-   		else
-	   		detailLayout.show(detailPanel, DOMAIN_VIEW);
-   	}
+    public void currentDomainChanged(DomainDisplayEvent e) {
+        if (e.getDomain() == null)
+            detailLayout.show(detailPanel, EMPTY_VIEW);
+        else
+            detailLayout.show(detailPanel, DOMAIN_VIEW);
+    }
 
-   	public void currentDataNodeChanged(DataNodeDisplayEvent e)
-   	{
-   		if (e.getDataNode() == null)
-	   		detailLayout.show(detailPanel, EMPTY_VIEW);
-   		else
-   			detailLayout.show(detailPanel, NODE_VIEW);
-   	}
-    
+    public void currentDataNodeChanged(DataNodeDisplayEvent e) {
+        if (e.getDataNode() == null)
+            detailLayout.show(detailPanel, EMPTY_VIEW);
+        else
+            detailLayout.show(detailPanel, NODE_VIEW);
+    }
 
-   	public void currentDataMapChanged(DataMapDisplayEvent e)
-   	{
-   		if (e.getDataMap() == null)
-	   		detailLayout.show(detailPanel, EMPTY_VIEW);
-   		else
-	   		detailLayout.show(detailPanel, DATA_MAP_VIEW);
-   	}
-    
-   	public void currentObjEntityChanged(EntityDisplayEvent e)
-   	{
-   		if (e.getEntity() == null)
-	   		detailLayout.show(detailPanel, EMPTY_VIEW);
-   		else
-	   		detailLayout.show(detailPanel, OBJ_VIEW);
-   	}
+    public void currentDataMapChanged(DataMapDisplayEvent e) {
+        if (e.getDataMap() == null)
+            detailLayout.show(detailPanel, EMPTY_VIEW);
+        else
+            detailLayout.show(detailPanel, DATA_MAP_VIEW);
+    }
 
+    public void currentObjEntityChanged(EntityDisplayEvent e) {
+        if (e.getEntity() == null)
+            detailLayout.show(detailPanel, EMPTY_VIEW);
+        else
+            detailLayout.show(detailPanel, OBJ_VIEW);
+    }
 
-   	public void currentDbEntityChanged(EntityDisplayEvent e)
-   	{
-   		if (e.getEntity() == null)
-	   		detailLayout.show(detailPanel, EMPTY_VIEW);
-   		else
-	   		detailLayout.show(detailPanel, DB_VIEW);
-   	}
+    public void currentDbEntityChanged(EntityDisplayEvent e) {
+        if (e.getEntity() == null)
+            detailLayout.show(detailPanel, EMPTY_VIEW);
+        else
+            detailLayout.show(detailPanel, DB_VIEW);
+    }
 }
