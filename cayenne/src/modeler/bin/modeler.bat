25c82242f8a7a3c231a974baa0434d930d85a923
Andrus Adamchik <aadamchik@apache.org>
2003-01-04 05:49:02 +0000

    made CayenneModeler startup scripts "smarter" not too sophisticated, but requiring less configuration. Now they can start with no CAYENNE_HOME defined
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@401575 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/src/modeler/bin/modeler.bat
+++ cayenne/src/modeler/bin/modeler.bat
@@ -2,6 +2,9 @@
 
 rem -------------------------------------------------------------------
 rem     Windows batch script to run Cayenne Modeler
+rem
+rem  Certain parts are modeled after Tomcat startup scrips, 
+rem  Copyright Apache Software Foundation
 rem -------------------------------------------------------------------
 
 
@@ -9,8 +12,13 @@ if not "%JAVA_HOME%" == "" goto check_cayenne_home
 echo Please define JAVA_HOME to point to your JSDK installation.
 goto eof
 
+
 :check_cayenne_home
-if not "%CAYENNE_HOME%" == "" goto run_modeler
+if not "%CAYENNE_HOME%" == "" goto got_home
+set CAYENNE_HOME=..
+
+:got_home
+if exist "%CAYENNE_HOME%\bin\modeler.bat" goto run_modeler
 echo Please define CAYENNE_HOME to point to your Cayenne installation.
 goto eof
 
--- cayenne/src/modeler/bin/modeler.sh
+++ cayenne/src/modeler/bin/modeler.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 
 # Bourne shell script to start Cayenne Modeler.
+#
+# Certain parts are modeled after Tomcat startup scrips, 
+# Copyright Apache Software Foundation
+
+# OS specific support.
+cygwin=false
+case "`uname`" in
+CYGWIN*) cygwin=true;;
+esac
 
 if [ "$JAVA_HOME" = "" ] ; then 
     echo "Please define JAVA_HOME to point to your JSDK installation."
     exit 1
 fi
 
-
 if [ "$CAYENNE_HOME" = "" ] ; then
+	CAYENNE_HOME=..
+fi
+
+if [ ! -f $CAYENNE_HOME/bin/modeler.sh ] ; then
     echo "Please define CAYENNE_HOME to point to your Cayenne installation."
     exit 1
 fi
