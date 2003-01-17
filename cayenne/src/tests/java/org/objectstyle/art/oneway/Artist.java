b0850da7425e15d729edecd184feb5e2276721e5
h2o-objectstyle <dev-null@apache.org>
2003-01-17 15:14:20 +0000

    initial event handling for DataObject
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@401609 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/src/tests/java/org/objectstyle/art/oneway/Artist.java
+++ cayenne/src/tests/java/org/objectstyle/art/oneway/Artist.java
@@ -1,7 +1,37 @@
+
 package org.objectstyle.art.oneway;
 
-public class Artist extends _Artist {
+import org.objectstyle.cayenne.event.DataObjectTransactionEvents;
 
+public class Artist extends _Artist implements DataObjectTransactionEvents
+{
+	private boolean _receivedWillCommit = false;
+	private boolean _receivedDidCommit = false;
+
+	public Artist()
+	{
+		super();
+	}
+	
+	public void didCommit()
+	{
+		_receivedDidCommit = true;
+	}
+
+	public void willCommit()
+	{
+		_receivedWillCommit = true;
+	}
+
+	public boolean receivedDidCommit()
+	{
+		return _receivedDidCommit;
+	}
+	
+	public boolean receivedWillCommit()
+	{
+		return _receivedWillCommit;
+	}
 }
 
 
