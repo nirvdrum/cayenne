c692d2a70cf632ab65b6029592ecf5a043dd44c6
h2o-objectstyle <dev-null@apache.org>
2003-01-18 22:52:05 +0000

    cleanups, more tests, fixed existing ones that didn't test anything
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@401615 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/src/tests/java/org/objectstyle/cayenne/event/InvocationTst.java
+++ cayenne/src/tests/java/org/objectstyle/cayenne/event/InvocationTst.java
@@ -59,35 +59,25 @@ package org.objectstyle.cayenne.event;
 import java.lang.reflect.Method;
 import java.util.HashSet;
 
-import org.apache.log4j.BasicConfigurator;
 import org.objectstyle.cayenne.unittest.CayenneTestCase;
 
 import junit.framework.Assert;
 
-public class InvocationTst extends CayenneTestCase
-{
+public class InvocationTst extends CayenneTestCase {
 	private Method _method;
-	
-	static
-	{
-		BasicConfigurator.configure();	
-	}
 
-	public InvocationTst(String arg0) throws NoSuchMethodException
-	{
+	public InvocationTst(String arg0) throws NoSuchMethodException {
 		super(arg0);
 		_method = this.getClass().getMethod("method", null);
 	}
 
-	public void testReflexive() throws Exception
-	{
-		Invocation inv = new Invocation(this, _method);
-		
+	public void testReflexive() throws Exception {
+		Invocation inv = new Invocation(this, _method);		
+
 		Assert.assertEquals(inv, inv);
 	}
 
-	public void testSymmetric() throws Exception
-	{
+	public void testSymmetric() throws Exception {
 		Invocation inv = new Invocation(this, _method);
 		Invocation inv2 = new Invocation(this, _method);
 		
@@ -95,8 +85,7 @@ public class InvocationTst extends CayenneTestCase
 		Assert.assertEquals(inv2, inv);
 	}
 
-	public void testTransitive() throws Exception
-	{
+	public void testTransitive() throws Exception {
 		Invocation inv = new Invocation(this, _method);
 		Invocation inv2 = new Invocation(this, _method);
 		Invocation inv3 = new Invocation(this, _method);
@@ -105,15 +94,13 @@ public class InvocationTst extends CayenneTestCase
 		Assert.assertEquals(inv2, inv3);
 		Assert.assertEquals(inv, inv3);
 	}
-		
-	public void testNull()
-	{
+
+	public void testNull() {
 		Invocation inv = new Invocation(this, _method);
 		Assert.assertTrue(inv.equals(null) == false);
 	}
-	
-	public void testDifferentMethods() throws Exception 
-	{
+
+	public void testDifferentMethods() throws Exception  {
 		Method m2 = this.getClass().getMethod("anotherMethod", null);
 
 		Invocation inv = new Invocation(this, _method);
@@ -122,8 +109,7 @@ public class InvocationTst extends CayenneTestCase
 		Assert.assertTrue(inv.equals(inv2) == false);
 	}
 
-	public void testAddToSet() throws Exception
-	{
+	public void testAddToSet() throws Exception {
 		HashSet set = new HashSet();
 		
 		Invocation inv = new Invocation(this, _method);
@@ -131,9 +117,8 @@ public class InvocationTst extends CayenneTestCase
 		set.add(inv);
 		Assert.assertEquals(1, set.size());
 	}
-	
-	public void testAddTwo()
-	{
+
+	public void testAddTwo() {
 		HashSet set = new HashSet();
 		
 		Invocation inv = new Invocation(this, _method);
@@ -144,8 +129,7 @@ public class InvocationTst extends CayenneTestCase
 		Assert.assertEquals(1, set.size());
 	}
 
-	public void testGarbageCollection() throws NoSuchMethodException
-	{
+	public void testGarbageCollection() throws NoSuchMethodException {
 		// create an invocation with an observer that will be garbage collected
 		Invocation inv = new Invocation(new String(), String.class.getMethod("toString", null));
 		
@@ -157,11 +141,9 @@ public class InvocationTst extends CayenneTestCase
 	}
 	
 	// these methods exist for the test of Invocation equality
-	public void method()
-	{
+	public void method() {
 	}
-	
-	public void anotherMethod()
-	{
+
+	public void anotherMethod() {
 	}
 }
--- cayenne/src/tests/java/org/objectstyle/cayenne/event/ObserverManagerTst.java
+++ cayenne/src/tests/java/org/objectstyle/cayenne/event/ObserverManagerTst.java
@@ -56,7 +56,6 @@
 
 package org.objectstyle.cayenne.event;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.objectstyle.cayenne.unittest.CayenneTestCase;
 
@@ -68,94 +67,76 @@ public class ObserverManagerTst extends CayenneTestCase
 	
 	private ObserverManager _observerManager;
 	private boolean _didReceiveNotification;
-
-	static
-	{
-		BasicConfigurator.configure();	
-	}
 	
-	public ObserverManagerTst(String arg0)
-	{
+	public ObserverManagerTst(String arg0) {
 		super(arg0);
 	}
-	
-	public void setUp() throws Exception
-	{
+
+	public void setUp() throws Exception {
 		_observerManager = new ObserverManager();
 		_didReceiveNotification = false;
 	}
 
-	public void testNullObserver() throws NoSuchMethodException
-	{
-		try
-		{
+	public void testNullObserver() throws NoSuchMethodException {
+		try {
 			ObserverSubject subject = ObserverSubject.getSubject(this.getClass(), "hansi");
 			_observerManager.addObserver(null, null, subject);
 			Assert.fail();
 		}
 
-		catch (IllegalArgumentException ia)
-		{
+		catch (IllegalArgumentException ia) {
 			// OK: argument is supposed to be illegal
 		}
 	}
-	
-	public void testNullNotification()
-	{
+
+	public void testNullNotification() {
 		// null notification
-		try
-		{
+		try {
 			_observerManager.addObserver(this, "testNullObserver", null);
 			Assert.fail();
 		}
-		catch (Exception e) 
-		{	
+
+		catch (Exception e) {	
 			Assert.assertTrue(e instanceof IllegalArgumentException);
 		}
-		
+
 		// empty string notification
-		try
-		{
+		try {
 			ObserverSubject subject = ObserverSubject.getSubject(this.getClass(), "");		
 			_observerManager.addObserver(this, "testNullObserver", subject);
 			Assert.fail();
 		}
-		catch (Exception e)
-		{
+
+		catch (Exception e) {
 			Assert.assertTrue(e instanceof IllegalArgumentException);
 		}
 	}
-	
-	public void testNonexistingMethod()
-	{
-		try
-		{
+
+	public void testNonexistingMethod() {
+		try {
 			ObserverSubject subject = ObserverSubject.getSubject(this.getClass(), "hansi");		
 			_observerManager.addObserver(this, "thisMethodDoesNotExist", subject);
 			Assert.fail();
 		}
-		catch (Exception e)
-		{
+
+		catch (Exception e) {
 			Assert.assertTrue(e instanceof NoSuchMethodException);
 		}
 	}
 	
-	public void testInvalidArgumentTypes()
-	{
-		try
-		{
+	public void testInvalidArgumentTypes() {
+		try {
 			ObserverSubject subject = ObserverSubject.getSubject(this.getClass(), "hansi");		
 			_observerManager.addObserver(this, "seeTheWrongMethod", subject);
 			Assert.fail();
 		}
-		catch (Exception e)
-		{
+
+		catch (Exception e) {
 			Assert.assertTrue(e instanceof NoSuchMethodException);
 		}
 	}
-	
-	public void testNonretainedObserver() throws NoSuchMethodException
-	{
+
+	public void testNonretainedObserver() throws NoSuchMethodException {
 		ObserverSubject subject = ObserverSubject.getSubject(this.getClass(), "XXX");
 		_observerManager.addObserver(new ObserverManagerTst(""), "seeNotification", subject);
 		
@@ -163,54 +144,54 @@ public class ObserverManagerTst extends CayenneTestCase
 		_observerManager.postObserverEvent(subject, this);
 	}
 	
-	public void testSuccessfulNotification()
-	{
-		try
-		{
+	public void testSuccessfulNotification() {
+		try {
 			ObserverSubject subject = ObserverSubject.getSubject(this.getClass(), "XXX");		
 			_observerManager.addObserver(this, "seeNotification", subject);
 			_observerManager.postObserverEvent(subject, this);
 			
 			Assert.assertTrue(_didReceiveNotification);
 		}
-		catch (Exception e)
-		{
+
+		catch (Exception e) {
 			log.error("testSuccessfulNotification", e);
 			Assert.fail();
 		}
 	}
-	
-	public void testRemoveOnEmptyList()
-	{
+
+	public void testRemoveOnEmptyList() {
 		ObserverSubject subject = ObserverSubject.getSubject(this.getClass(), "XXX");		
-		_observerManager.removeObserver(this, subject);
-	}
-	
-	public void testRemoveOnNullObserverSubject()
-	{
-		Assert.assertEquals(false, _observerManager.removeObserver(this, null));
-	}
-	
-	public void testRemove() throws NoSuchMethodException
-	{
-		ObserverSubject subject = ObserverSubject.getSubject(this.getClass(), "XXX");		
-		_observerManager.addObserver(this, "seeNotification", subject);
-		
-		boolean didRemove = _observerManager.removeObserver(this, subject);
-		Assert.assertTrue(didRemove);
+		Assert.assertEquals(false, _observerManager.removeObserver(this, subject));
 	}
 
-	//-------------------------------------------------------------------------
+	public void testRemoveOnNullObserverSubject() {
+		Assert.assertEquals(false, _observerManager.removeObserver(this, null));
+	}
+
+	public void testRemove() throws NoSuchMethodException {
+		ObserverSubject subject = ObserverSubject.getSubject(this.getClass(), "XXX");		
+		_observerManager.addObserver(this, "seeNotification", subject);
+		Assert.assertTrue(_observerManager.removeObserver(this, subject));
+	}
+
+	public void testRemoveAll() throws NoSuchMethodException {
+		ObserverSubject subject1 = ObserverSubject.getSubject(this.getClass(), "XXX1");
+		ObserverSubject subject2 = ObserverSubject.getSubject(this.getClass(), "XXX2");
+		_observerManager.addObserver(this, "seeNotification", subject1);
+		_observerManager.addObserver(this, "seeNotification", subject2);
+
+		Assert.assertTrue(_observerManager.removeObserver(this));
+		Assert.assertEquals(false, _observerManager.removeObserver(this, subject1));
+		Assert.assertEquals(false, _observerManager.removeObserver(this, subject2));
+	}
+
 	// notification method
-	//-------------------------------------------------------------------------
-	public void seeNotification(ObserverEvent event)
-	{
+	public void seeNotification(ObserverEvent event) {
 		log.debug("seeNotification. publisher: " + event.getPublisher().getClass().getName());
 		_didReceiveNotification = true;
 	}
-	
-	public void seeTheWrongMethod(int hansi)
-	{
+
+	public void seeTheWrongMethod(int hansi) {
 		log.debug("seeTheWrongMethod: " + hansi);
 	}
 }
