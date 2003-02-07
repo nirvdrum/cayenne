55ef588c7ad7737c631bad4af472ae2ff8a12d8f
Andrus Adamchik <aadamchik@apache.org>
2003-02-07 02:18:08 +0000

    added date tests
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@401769 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/src/tests/java/org/objectstyle/cayenne/access/DataContextDatesTst.java
+++ cayenne/src/tests/java/org/objectstyle/cayenne/access/DataContextDatesTst.java
@@ -53,93 +53,81 @@
  * <http://objectstyle.org/>.
  *
  */
- 
- package org.objectstyle.cayenne.access;
 
-import java.math.BigDecimal;
-import java.sql.Types;
-import java.util.List;
+package org.objectstyle.cayenne.access;
 
-import org.apache.log4j.Level;
-import org.objectstyle.art.Artist;
-import org.objectstyle.art.Painting;
-import org.objectstyle.cayenne.map.Procedure;
-import org.objectstyle.cayenne.query.ProcedureQuery;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.objectstyle.art.DateTest;
 import org.objectstyle.cayenne.query.SelectQuery;
 import org.objectstyle.cayenne.unittest.CayenneTestCase;
 
 /**
+ * Tests Date handling in Cayenne.
+ * 
  * @author Andrei Adamchik
  */
-public class DataContextStoredProcTst extends CayenneTestCase {
-    public static final String UPDATE_STORED_PROCEDURE = "cayenne_tst_upd_proc";
-    public static final String SELECT_STORED_PROCEDURE =
-        "cayenne_tst_select_proc";
-
+public class DataContextDatesTst extends CayenneTestCase {
     protected DataContext ctxt;
-    
     /**
-     * Constructor for DataContextStoredProcTst.
+     * Constructor for DataContextDatesTst.
      * @param name
      */
-    public DataContextStoredProcTst(String name) {
+    public DataContextDatesTst(String name) {
         super(name);
     }
 
-    public void testUpdate() throws Exception {
-        // Don't run this on MySQL
-        if (!getDatabaseSetupDelegate().supportsStoredProcedures()) {
-            return;
-        }
-        
-        
-        // create an artist with painting in the database  
-        createArtist(1000.0);
-        
-        // create and run stored procedure
-        Procedure proc = new Procedure(UPDATE_STORED_PROCEDURE);
-        proc.addParam("paintingPrice", Types.INTEGER);
-        
-        ProcedureQuery q = new ProcedureQuery(Artist.class, proc);
-        q.addParam("paintingPrice", new Integer(3000));
-        DefaultOperationObserver observer = new DefaultOperationObserver();
-        observer.setLoggingLevel(Level.WARN);
-        ctxt.performQuery(q, observer);
-        
-        // check that price have doubled
-        SelectQuery select = new SelectQuery(Artist.class);
-        select.addPrefetch("paintingArray");
-        select.setLoggingLevel(Level.WARN);
-        List artists = ctxt.performQuery(select);
-        assertEquals(1, artists.size());
-        
-        Artist a = (Artist)artists.get(0);
-        Painting p = (Painting)a.getPaintingArray().get(0);
-        assertEquals(2000, p.getEstimatedPrice().intValue());
+    public void testDate() throws Exception {
+        DateTest test = (DateTest) ctxt.createAndRegisterNewObject("DateTest");
+
+        Calendar cal = Calendar.getInstance();
+        cal.clear();
+        cal.set(2002, 1, 1);
+        Date nowDate = cal.getTime();
+        test.setDateColumn(nowDate);
+        ctxt.commitChanges();
+
+        SelectQuery q = new SelectQuery(DateTest.class);
+        DateTest testRead = (DateTest) ctxt.performQuery(q).get(0);
+        assertNotNull(testRead.getDateColumn());
+        assertEquals(nowDate, testRead.getDateColumn());
     }
-    
-    
-    protected void createArtist(double paintingPrice) {
-    	Artist a = (Artist)ctxt.createAndRegisterNewObject("Artist");
-    	a.setArtistName("An Artist");
-    	
-    	Painting p = (Painting)ctxt.createAndRegisterNewObject("Painting");
-    	p.setPaintingTitle("A Painting");
-    	p.setEstimatedPrice(new BigDecimal(paintingPrice));
-    	a.addToPaintingArray(p);
-    	
-    	ctxt.commitChanges();
+
+    public void testTime() throws Exception {
+        DateTest test = (DateTest) ctxt.createAndRegisterNewObject("DateTest");
+
+        Calendar cal = Calendar.getInstance();
+        cal.clear();
+        cal.set(1970, 0, 1, 1, 20, 30);
+        Date nowTime = cal.getTime();
+        test.setTimeColumn(nowTime);
+        ctxt.commitChanges();
+
+        SelectQuery q = new SelectQuery(DateTest.class);
+        DateTest testRead = (DateTest) ctxt.performQuery(q).get(0);
+        assertNotNull(testRead.getTimeColumn());
+        assertEquals(nowTime, testRead.getTimeColumn());
     }
-    
-   
+
+    public void testTimestamp() throws Exception {
+        DateTest test = (DateTest) ctxt.createAndRegisterNewObject("DateTest");
+
+        Calendar cal = Calendar.getInstance();
+        cal.clear();
+        cal.set(2003, 1, 1, 1, 20, 30);
+        Date now = cal.getTime();
+        test.setTimestampColumn(now);
+        ctxt.commitChanges();
+
+        SelectQuery q = new SelectQuery(DateTest.class);
+        DateTest testRead = (DateTest) ctxt.performQuery(q).get(0);
+        assertNotNull(testRead.getTimestampColumn());
+        assertEquals(now, testRead.getTimestampColumn());
+    }
+
     protected void setUp() throws Exception {
-        // Don't run this on MySQL
-        if (!getDatabaseSetupDelegate().supportsStoredProcedures()) {
-            return;
-        }
-        
         getDatabaseSetup().cleanTableData();
         ctxt = createDataContext();
     }
-
 }
