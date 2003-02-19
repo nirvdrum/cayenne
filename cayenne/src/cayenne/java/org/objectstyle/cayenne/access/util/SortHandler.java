a96d6003fbc1bc294c8a5e99b4daff826f77485b
Andrus Adamchik <aadamchik@apache.org>
2003-02-19 04:52:54 +0000

    small update to the SortHandler
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@401829 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/src/cayenne/java/org/objectstyle/cayenne/access/util/SortHandler.java
+++ cayenne/src/cayenne/java/org/objectstyle/cayenne/access/util/SortHandler.java
@@ -1,3 +1,59 @@
+/* ====================================================================
+ *
+ * The ObjectStyle Group Software License, Version 1.0
+ *
+ * Copyright (c) 2002 The ObjectStyle Group
+ * and individual authors of the software.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        ObjectStyle Group (http://objectstyle.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "ObjectStyle Group" and "Cayenne"
+ *    must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact andrus@objectstyle.org.
+ *
+ * 5. Products derived from this software may not be called "ObjectStyle"
+ *    nor may "ObjectStyle" appear in their names without prior written
+ *    permission of the ObjectStyle Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE OBJECTSTYLE GROUP OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the ObjectStyle Group.  For more
+ * information on the ObjectStyle Group, please see
+ * <http://objectstyle.org/>.
+ *
+ */
+
 package org.objectstyle.cayenne.access.util;
 
 import java.util.Arrays;
@@ -9,7 +65,7 @@ import org.objectstyle.cayenne.access.QueryEngine;
 
 /**
  * Defines a set of sorting methods based on object dependencies. The actual
- * dependency tracking algorithm is pluggable via a sporter class.
+ * dependency tracking algorithm is pluggable via a sorter class.
  * 
  * @author Andrei Adamchik
  */
@@ -17,6 +73,8 @@ public class SortHandler {
     protected static Class defaultSorterClass = DefaultSorter.class;
 
     protected DependencySorter sorter;
+    protected Class sorterClass;
+    protected QueryEngine queryEngine;
 
     public static Class getDefaultSorterClass() {
         return defaultSorterClass;
@@ -36,39 +94,42 @@ public class SortHandler {
     /**
      * Constructor for SortHandler.
      */
-    public SortHandler(QueryEngine queryEngine, List dataMaps) {
-        this(defaultSorterClass, queryEngine, dataMaps);
+    public SortHandler(QueryEngine queryEngine) {
+        this(defaultSorterClass, queryEngine);
     }
 
     /**
       * Creates SortHandler with a specified sorter class.
       */
-    public SortHandler(
-        Class sorterClass,
-        QueryEngine queryEngine,
-        List dataMaps) {
-
+    public SortHandler(Class sorterClass, QueryEngine queryEngine) {
         // sanity check
         if (sorterClass == null) {
             throw new IllegalArgumentException("DependencySorter class can not be null.");
         }
-
-        if (!DependencySorter.class.isAssignableFrom(sorterClass)) {
-            throw new IllegalArgumentException(
-                "Sorter class "
-                    + sorterClass.getName()
-                    + " must implement DependencySorter interface.");
+        if (queryEngine == null) {
+            throw new IllegalArgumentException("QueryEngine can not be null.");
         }
 
+        this.sorterClass = sorterClass;
+        this.queryEngine = queryEngine;
+        
+        indexSorter();
+    }
+
+    public void indexSorter() {
+        DependencySorter newSorter;
+
         try {
-            sorter = (DependencySorter) sorterClass.newInstance();
+            newSorter = (DependencySorter) sorterClass.newInstance();
+
         } catch (Exception ex) {
             throw new CayenneRuntimeException(
                 "Error instantiating sorter from " + sorterClass.getName(),
                 ex);
         }
 
-        sorter.initSorter(queryEngine, dataMaps);
+        newSorter.initSorter(queryEngine, queryEngine.getDataMapsAsList());
+        this.sorter = newSorter;
     }
 
     /**
