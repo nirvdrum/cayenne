deccbc764bfbd4358dbdf46aeb43149c0b39acbd
Andrus Adamchik <aadamchik@apache.org>
2003-03-20 02:26:30 +0000

    added meaningful PK to the test entity
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@402014 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/src/tests/java/org/objectstyle/art/CompoundPkTest.java
+++ cayenne/src/tests/java/org/objectstyle/art/CompoundPkTest.java
@@ -5,6 +5,22 @@ import org.objectstyle.cayenne.*;
 
 public class CompoundPkTest extends org.objectstyle.cayenne.CayenneDataObject {
 
+    public void setKey1(String key1) {
+        writeProperty("key1", key1);
+    }
+    public String getKey1() {
+        return (String)readProperty("key1");
+    }
+    
+    
+    public void setKey2(String key2) {
+        writeProperty("key2", key2);
+    }
+    public String getKey2() {
+        return (String)readProperty("key2");
+    }
+    
+    
     public void setName(String name) {
         writeProperty("name", name);
     }
