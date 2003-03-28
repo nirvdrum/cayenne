1b84b49a2cd56c254613a4275ec917d68a16520d
Andrus Adamchik <aadamchik@apache.org>
2003-03-28 04:29:29 +0000

    fixed Oracle stored procedure DDL scripts
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@402044 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/src/tests/resources/test-resources/ddl/oracle/create-select-sp.sql
+++ cayenne/src/tests/resources/test-resources/ddl/oracle/create-select-sp.sql
@@ -1,17 +1,19 @@
- CREATE OR REPLACE PROCEDURE cayenne_tst_select_proc
- (a_name IN VARCHAR2, painting_price IN NUMBER) AS
+CREATE OR REPLACE FUNCTION cayenne_tst_select_proc (a_name IN VARCHAR2, painting_price IN NUMBER)
+    RETURN cayenne_types.ref_cursor
+AS
    artists cayenne_types.ref_cursor;
- BEGIN
-        SET TRANSACTION READ WRITE;
-        UPDATE PAINTING SET ESTIMATED_PRICE = ESTIMATED_PRICE * 2
-        WHERE ESTIMATED_PRICE < painting_price;
-        COMMIT;
-        
-        OPEN artists FOR
-        SELECT DISTINCT A.ARTIST_ID, A.DATE_OF_BIRTH, A.ARTIST_NAME
-        FROM ARTIST A, PAINTING P
-        WHERE A.ARTIST_ID = P.ARTIST_ID AND
-        A.ARTIST_NAME = a_name;
-        
-        return artists;
- END;
+BEGIN
+      SET TRANSACTION READ WRITE;
+      UPDATE PAINTING SET ESTIMATED_PRICE = ESTIMATED_PRICE * 2
+      WHERE ESTIMATED_PRICE < painting_price;
+      COMMIT;
+ 
+     OPEN artists FOR
+     SELECT DISTINCT A.ARTIST_ID, A.DATE_OF_BIRTH, A.ARTIST_NAME
+     FROM ARTIST A, PAINTING P
+     WHERE A.ARTIST_ID = P.ARTIST_ID AND
+     A.ARTIST_NAME = a_name;
+
+     RETURN artists;
+END;
+
