37f64762d185509a9bde3cbec6d9e7a467332a3a
h2o-objectstyle <dev-null@apache.org>
2003-05-21 15:05:39 +0000

    initial work on DB2 adapter - NOT yet ready!
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@402202 13f79535-47bb-0310-9956-ffa450edef68

---
+++ cayenne/src/cayenne/java/org/objectstyle/cayenne/dba/db2/DB2PkGenerator.java
@@ -0,0 +1,192 @@
+/* ====================================================================
+ * 
+ * The ObjectStyle Group Software License, Version 1.0 
+ *
+ * Copyright (c) 2002-2003 The ObjectStyle Group 
+ * and individual authors of the software.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:  
+ *       "This product includes software developed by the 
+ *        ObjectStyle Group (http://objectstyle.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "ObjectStyle Group" and "Cayenne" 
+ *    must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written 
+ *    permission, please contact andrus@objectstyle.org.
+ *
+ * 5. Products derived from this software may not be called "ObjectStyle"
+ *    nor may "ObjectStyle" appear in their names without prior written
+ *    permission of the ObjectStyle Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE OBJECTSTYLE GROUP OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the ObjectStyle Group.  For more
+ * information on the ObjectStyle Group, please see
+ * <http://objectstyle.org/>.
+ *
+ */ 
+
+package org.objectstyle.cayenne.dba.db2;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.objectstyle.cayenne.CayenneRuntimeException;
+import org.objectstyle.cayenne.access.DataNode;
+import org.objectstyle.cayenne.dba.JdbcPkGenerator;
+import org.objectstyle.cayenne.map.DbEntity;
+
+/**
+ * PK Generator for IBM DB2 using sequences.
+ * 
+ * @author Mario Linke
+ */
+public class DB2PkGenerator extends JdbcPkGenerator {
+
+	public static final String SEQUENCE_PREFIX = "S_";
+	
+	public List dropAutoPkStatements(List dbEntities) {
+		 List list = new ArrayList();
+		 Iterator it = dbEntities.iterator();
+		 while (it.hasNext()) {
+			 DbEntity ent = (DbEntity) it.next();
+			 list.add(dropSequenceString(ent));
+		 }
+
+		 return list;
+	 }	
+	
+	/** Gibt den Sequence-Namen anhand des Tabellennamen wieder */
+	protected String sequenceName(DbEntity ent) {
+		String entName = ent.getName();
+		String seqName = SEQUENCE_PREFIX + entName.toLowerCase();
+
+		if (ent.getSchema() != null && ent.getSchema().length() > 0) {
+			seqName = ent.getSchema() + "." + seqName;
+		}
+
+		return seqName;
+	}	
+	
+	
+	/** Erzeugt den SQL-String zum erzeugen von Sequencen	 */	  	
+	protected String createSequenceString(DbEntity ent) {
+		StringBuffer buf = new StringBuffer();
+		buf
+			.append("CREATE SEQUENCE ")
+			.append(sequenceName (ent))
+			.append(" START WITH 1")
+			.append(" INCREMENT BY ")
+			.append(" NO MAXVALUE ")
+			.append(" NO CYCLE ")
+			.append(" CACHE ").append(getPkCacheSize());
+		return buf.toString();
+	}	
+	
+	/** Loeschen einer Sequence	 */
+	protected String dropSequenceString(DbEntity ent) {
+		StringBuffer buf = new StringBuffer();
+		buf.append("DROP SEQUENCE ").append(sequenceName(ent)).append(" RESTRICT ");
+		return buf.toString();
+	}	
+	
+	/**
+	 * Erzeugt einen neuen Pk anhand der Seqeunce
+	 * <pre>
+	 * SELECT pk_table_name.nextval FROM SYSIBM.SYSDUMMY1 
+	 * </pre>
+	 * SYSIBM.SYSDUMMY1 entspricht DUAL von Oracle
+	 */
+	protected int pkFromDatabase(DataNode node, DbEntity ent) throws Exception {
+
+		String seq_name = sequenceName (ent);
+		Connection con = node.getDataSource().getConnection();
+		try {
+		  Statement st = con.createStatement();
+		  try {
+			ResultSet rs = st.executeQuery( "SELECT NEXTVAL FOR "
+				+ seq_name	+ " FROM SYSIBM.SYSDUMMY1");
+			try {
+			  //Object pk = null;
+			  if (!rs.next()) {
+				throw new CayenneRuntimeException(
+					"Fehler in pkFromDatabase() fuer Tabelle "
+					+ ent.getName() + " / Sequence " + seq_name);
+			  }
+			  return rs.getInt(1);
+			} finally {
+			  rs.close();
+			}
+		  } finally {
+			st.close();
+		  }
+		} finally {
+		  con.close();
+		}
+	}	
+	
+	
+	/** gibt alle vorhandenen Sequencen wieder	 */
+	protected List getExistingSequences(DataNode node) throws SQLException {
+
+		// check existing sequences
+		Connection con = node.getDataSource().getConnection();
+
+		try {
+			Statement sel = con.createStatement();
+			try {
+				StringBuffer q = new StringBuffer();
+				q.append(
+					"SELECT LOWER(SEQNAME) FROM SYSCAT.SEQUENCES WHERE LOWER(SEQUNAME)");
+				q.append(" LIKE '").append(SEQUENCE_PREFIX).append("%'");
+				ResultSet rs = sel.executeQuery(q.toString());
+				try {
+					List sequenceList = new ArrayList();
+					while (rs.next()) {
+						sequenceList.add(rs.getString(1));
+					}
+					return sequenceList;
+				} finally {
+					rs.close();
+				}
+			} finally {
+				sel.close();
+			}
+		} finally {
+			con.close();
+		}
+	}	
+}
