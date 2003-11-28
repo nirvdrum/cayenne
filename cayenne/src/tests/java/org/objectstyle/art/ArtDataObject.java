1fdffecaf18025e58c125dd5a47ba16b2efe4838
Andrus Adamchik <aadamchik@apache.org>
2003-11-28 22:31:26 +0000

    added license
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@402731 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/src/tests/java/org/objectstyle/art/ArtDataObject.java
+++ cayenne/src/tests/java/org/objectstyle/art/ArtDataObject.java
@@ -1,4 +1,3 @@
-package org.objectstyle.art;
 /* ====================================================================
  * 
  * The ObjectStyle Group Software License, Version 1.0 
@@ -54,6 +53,7 @@ package org.objectstyle.art;
  * <http://objectstyle.org/>.
  *
  */ 
+package org.objectstyle.art;
 
 import org.objectstyle.cayenne.CayenneDataObject;
 
