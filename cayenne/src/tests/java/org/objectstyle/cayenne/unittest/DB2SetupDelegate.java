0ad03f58b896677d58d43965074c64ee354889c7
Andrus Adamchik <aadamchik@apache.org>
2003-12-22 04:35:43 +0000

    adding SP tests
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@402840 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/src/tests/java/org/objectstyle/cayenne/unittest/DB2SetupDelegate.java
+++ cayenne/src/tests/java/org/objectstyle/cayenne/unittest/DB2SetupDelegate.java
@@ -1,11 +1,17 @@
 package org.objectstyle.cayenne.unittest;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.log4j.Logger;
 import org.objectstyle.cayenne.dba.DbAdapter;
+import org.objectstyle.cayenne.map.DataMap;
 
 /**
  * @author Andrei Adamchik
  */
 public class DB2SetupDelegate extends DatabaseSetupDelegate {
+    private static Logger logObj = Logger.getLogger(DB2SetupDelegate.class);
 
     public DB2SetupDelegate(DbAdapter adapter) {
         super(adapter);
@@ -14,8 +20,43 @@ public class DB2SetupDelegate extends DatabaseSetupDelegate {
     public boolean supportsBinaryPK() {
         return false;
     }
-    
+
     public boolean supportsLobs() {
         return true;
+    }
+
+    public boolean supportsStoredProcedures() {
+        return false;
+    }
+
+    public void createdTables(Connection con, DataMap map) throws Exception {
+        executeDDL(con, super.ddlFile("db2", "create-update-sp.sql"));
+        executeDDL(con, super.ddlFile("db2", "create-out-sp.sql"));
+        executeDDL(con, super.ddlFile("db2", "create-select-sp.sql"));
+    }
+
+    public void willDropTables(Connection con, DataMap map) throws Exception {
+        // still have to figure out how to safely drop procedures
+
+        try {
+            executeDDL(con, super.ddlFile("db2", "drop-select-sp.sql"));
+        }
+        catch (SQLException ex) {
+            logObj.info("Can't drop procedure, ignoring.", ex);
+        }
+
+        try {
+            executeDDL(con, super.ddlFile("db2", "drop-update-sp.sql"));
+        }
+        catch (SQLException ex) {
+            logObj.info("Can't drop procedure, ignoring.", ex);
+        }
+
+        try {
+            executeDDL(con, super.ddlFile("db2", "drop-out-sp.sql"));
+        }
+        catch (SQLException ex) {
+            logObj.info("Can't drop procedure, ignoring.", ex);
+        }
     }
 }
