b1eb311aff6996f5ec74aea22517cd7a48ecc150
Andrus Adamchik <aadamchik@apache.org>
2004-01-17 04:55:06 +0000

    changed Copyright year to 2004
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@402871 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/src/tests/java/org/objectstyle/cayenne/map/ProcedureTst.java
+++ cayenne/src/tests/java/org/objectstyle/cayenne/map/ProcedureTst.java
@@ -4,7 +4,7 @@ package org.objectstyle.cayenne.map;
  * 
  * The ObjectStyle Group Software License, Version 1.0 
  *
- * Copyright (c) 2002-2003 The ObjectStyle Group 
+ * Copyright (c) 2002-2004 The ObjectStyle Group 
  * and individual authors of the software.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
