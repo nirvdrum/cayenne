0e5e8d1a090b046776006f8e05750dd9c7464d21
Andrus Adamchik <aadamchik@apache.org>
2004-02-26 04:35:33 +0000

    added testcase for stored queries
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@403040 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/src/tests/java/org/objectstyle/cayenne/map/MapLoaderLoadTst.java
+++ cayenne/src/tests/java/org/objectstyle/cayenne/map/MapLoaderLoadTst.java
@@ -59,53 +59,92 @@ import java.util.List;
 
 import junit.framework.TestCase;
 
+import org.objectstyle.art.Artist;
+import org.objectstyle.art.Gallery;
+import org.objectstyle.cayenne.query.Ordering;
+import org.objectstyle.cayenne.query.SelectQuery;
 import org.objectstyle.cayenne.unittest.CayenneTestResources;
 import org.xml.sax.InputSource;
-
 
 public class MapLoaderLoadTst extends TestCase {
     protected MapLoader mapLoader;
     private String testDataMap;
 
-
     public void setUp() throws Exception {
         mapLoader = new MapLoader();
-        testDataMap = ClassLoader.getSystemResource(CayenneTestResources.TEST_MAP_PATH).toExternalForm();
+        testDataMap =
+            ClassLoader
+                .getSystemResource(CayenneTestResources.TEST_MAP_PATH)
+                .toExternalForm();
     }
-
 
     public void testLoadDataMap() throws Exception {
         InputSource in = new InputSource(testDataMap);
         DataMap map = mapLoader.loadDataMap(in);
         assertNotNull(map);
-        
+
         // test procedures
         Procedure procedure = map.getProcedure("cayenne_tst_upd_proc");
         assertNotNull(procedure);
         List params = procedure.getCallParameters();
         assertNotNull(params);
         assertEquals(1, params.size());
-        ProcedureParameter param = (ProcedureParameter)params.get(0);
+        ProcedureParameter param = (ProcedureParameter) params.get(0);
         assertNotNull(param);
         assertEquals("paintingPrice", param.getName());
         assertEquals(ProcedureParameter.IN_PARAMETER, param.getDirection());
-        
+
         // test derived entities
-        DerivedDbEntity d1 = (DerivedDbEntity)map.getDbEntity("ARTIST_ASSETS");
+        DerivedDbEntity d1 = (DerivedDbEntity) map.getDbEntity("ARTIST_ASSETS");
         assertNotNull(d1);
         assertNotNull(d1.getParentEntity());
         assertEquals(1, d1.getGroupByAttributes().size());
-        
-        DerivedDbAttribute a1 = (DerivedDbAttribute)d1.getAttribute("ESTIMATED_PRICE");
+
+        DerivedDbAttribute a1 = (DerivedDbAttribute) d1.getAttribute("ESTIMATED_PRICE");
         assertNotNull(a1);
         assertNotNull(a1.getExpressionSpec());
         assertNotNull(a1.getParams());
-        assertEquals(1, a1.getParams().size());        
-        
+        assertEquals(1, a1.getParams().size());
+
         //test super class name
         //We expect the artist entity to have a super class name... test map should be set up in that way.
         //No other assertions can be made (the actual super class may change)
-        ObjEntity ent=map.getObjEntity("Painting");
+        ObjEntity ent = map.getObjEntity("Painting");
         assertNotNull(ent.getSuperClassName());
+
+        checkLoadedQueries(map);
+    }
+
+    private void checkLoadedQueries(DataMap map) throws Exception {
+        SelectQuery queryWithQualifier = (SelectQuery) map.getQuery("QueryWithQualifier");
+        assertNotNull(queryWithQualifier);
+        assertTrue(queryWithQualifier.getRoot() instanceof ObjEntity);
+        assertEquals("Artist", ((Entity) queryWithQualifier.getRoot()).getName());
+        assertNotNull(queryWithQualifier.getQualifier());
+
+        SelectQuery queryWithOrdering = (SelectQuery) map.getQuery("QueryWithOrdering");
+        assertNotNull(queryWithOrdering);
+        assertTrue(queryWithOrdering.getRoot() instanceof ObjEntity);
+        assertEquals("Artist", ((Entity) queryWithOrdering.getRoot()).getName());
+        assertEquals(2, queryWithOrdering.getOrderings().size());
+
+        Ordering artistNameOrdering = (Ordering) queryWithOrdering.getOrderings().get(0);
+        assertEquals(Artist.ARTIST_NAME_PROPERTY, artistNameOrdering.getSortSpecString());
+        assertFalse(artistNameOrdering.isAscending());
+        assertTrue(artistNameOrdering.isCaseInsensitive());
+
+        Ordering dobOrdering = (Ordering) queryWithOrdering.getOrderings().get(1);
+        assertEquals(Artist.DATE_OF_BIRTH_PROPERTY, dobOrdering.getSortSpecString());
+        assertTrue(dobOrdering.isAscending());
+        assertFalse(dobOrdering.isCaseInsensitive());
+
+        SelectQuery queryWithPrefetch = (SelectQuery) map.getQuery("QueryWithPrefetch");
+        assertNotNull(queryWithPrefetch);
+        assertTrue(queryWithPrefetch.getRoot() instanceof ObjEntity);
+        assertEquals("Gallery", ((Entity) queryWithPrefetch.getRoot()).getName());
+        assertEquals(1, queryWithPrefetch.getPrefetches().size());
+        assertEquals(
+            Gallery.PAINTING_ARRAY_PROPERTY,
+            queryWithPrefetch.getPrefetches().iterator().next());
     }
 }
