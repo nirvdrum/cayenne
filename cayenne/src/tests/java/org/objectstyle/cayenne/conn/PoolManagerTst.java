374b47a3fa95835254a91368cfa239af6f8d3d4e
Andrus Adamchik <aadamchik@apache.org>
2004-07-11 04:31:11 +0000

    reducing the number of connections used in the test to work with limited connection DB licneses... there is a leak somewhere that opens a second connection... otherwise this woud've been unnecessary
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/cayenne/main/trunk@403318 13f79535-47bb-0310-9956-ffa450edef68

--- cayenne/src/tests/java/org/objectstyle/cayenne/conn/PoolManagerTst.java
+++ cayenne/src/tests/java/org/objectstyle/cayenne/conn/PoolManagerTst.java
@@ -112,7 +112,7 @@ public class PoolManagerTst extends CayenneTestCase {
                 dsi.getJdbcDriver(),
                 dsi.getDataSourceUrl(),
                 2,
-                4,
+                3,
                 dsi.getUserName(),
                 dsi.getPassword());
 
@@ -128,21 +128,13 @@ public class PoolManagerTst extends CayenneTestCase {
             assertEquals(2, pm.getCurrentlyInUse());
             assertEquals(0, pm.getCurrentlyUnused());
 
-            Connection c3 = pm.getConnection();
-            assertEquals(3, pm.getCurrentlyInUse());
-            assertEquals(0, pm.getCurrentlyUnused());
-
-            c3.close();
-            assertEquals(2, pm.getCurrentlyInUse());
-            assertEquals(1, pm.getCurrentlyUnused());
-
             c1.close();
             assertEquals(1, pm.getCurrentlyInUse());
-            assertEquals(2, pm.getCurrentlyUnused());
+            assertEquals(1, pm.getCurrentlyUnused());
 
             c2.close();
             assertEquals(0, pm.getCurrentlyInUse());
-            assertEquals(3, pm.getCurrentlyUnused());
+            assertEquals(2, pm.getCurrentlyUnused());
         }
         finally {
             // get rid of local pool
